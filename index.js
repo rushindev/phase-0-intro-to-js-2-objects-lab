c13959f7ffc259c858baddeb1ebc399807098d51
York Rushin <rushinyork@gmail.com>
2022-06-26 21:49:27 -0700

    commit tree dc825d692421cba0c1333c7e986776af71679948

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Steve',
+    streetAddress: '31 Goliath'
+}
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, address) {
+    const newEmployee = { ...employee };
+    newEmployee[streetAddress] = address;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, address) {
+    employee[streetAddress] = address;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newerEmployee = { ...employee };
+    delete newerEmployee.name;
+    return newerEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
